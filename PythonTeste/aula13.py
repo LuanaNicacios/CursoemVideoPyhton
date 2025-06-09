839488ae985b5504c4260dc697ef2f33bdc8bf34
Luana Nicácio <luanavsnicacio@gmail.com>
2025-06-09 17:16:38 -0300

    MUNDO 2 PYTHON / AULA 13 - FOR AND RANGE

---
+++ PythonTeste/aula13.py
@@ -0,0 +1,54 @@
+# AULA DE FOR AND RANGE
+for oi in range (0, 10):
+    print('Olá, Mundo!')
+print('-FIM-')
+
+##Vai contar decrescente
+"""
+for c in range (6, 0, -1): #Vai contar decrescente
+    print(c)
+print('-FIM-')
+"""
+
+#Ele vai contar decrescente pulando 2 casa 0, 2, 4, 6 etc....
+"""
+for c in range (0, 7, 2) #Ele vai contar decrescente pulando 2 casa 0, 2, 4, 6 etc....
+    print(c)
+print('-FIM-')
+"""
+
+"""
+n =  int(input('Digite um numero: '))
+    print(c)
+print('-FIM-')
+"""
+
+##Vai contar do número que você quiser,
+# e vai pular quantas casas você quiser até o fim.
+"""
+inicio = int(input('Início: '))
+fim = int(input('Fim: '))'))
+passo = int(input('Passo: '))
+for c in range(inicio, fim+1, passo): #Vai contar do número que você quiser, e vai pular quantas casas você quiser até o fim.
+    print(c)
+print('-FIM-')
+"""
+
+"""
+for c in range (0, 3)
+    n = int(input('Digite um valor: ')) # VAI LER O NÚMERO 3 VEZES, PODE MUDAR Q A QUANTIDADE DE NÚMEROS
+print('-FIM-)
+"""
+
+#Vai somar todos os 4 números requeridos
+"""
+s = 0
+for c in range (0, 4):
+    n = int(input('Digite um valor: ')
+    s += n
+print('O somatório de todos os valores foi {}'.format(s))
+"""
+
+
+
+
